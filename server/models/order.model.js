e7ecf8c9b4f11c027009172aa21e46078bb137c6
Holson <philemonholson@gmail.com>
2022-05-18 18:51:24 -0400

    Starting order model

---
+++ server/models/order.model.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const OrderSchema = new mongoose.Schema({
+    products: [CartItemSchema],
+    customer_name: {
+      type: String,
+      trim: true,
+      required: 'Name is required'
+    },
+    customer_email: {
+      type: String,
+      trim: true,
+      match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+      required: 'Email is required'
+    },
+    delivery_address: {
+      street: {type: String, required: 'Street is required'},
+      city: {type: String, required: 'City is required'},
+      state: {type: String},
+      zipcode: {type: String, required: 'Zip Code is required'},
+      country: {type: String, required: 'Country is required'}
+    },
+    payment_id: {},
+    updated: Date,
+    created: {
+      type: Date,
+      default: Date.now
+    },
+    user: {type: mongoose.Schema.ObjectId, ref: 'User'}
+  })
+  
